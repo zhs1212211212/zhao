857b029b94b2c8d2cf54ba348614df55a927df13
zhs1212211212 <13188600650@163.com>
2026-08-16 20:33:50 +0800

    Add files via upload

--- pin-dou-tool/sw.js
+++ pin-dou-tool/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pindou-v1';
+const CACHE = 'pindou-v2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', (e) => {
@@ -15,6 +15,22 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
+
+  // 页面请求：网络优先（在线时总是拿最新版本，同时回写缓存）
+  if (e.request.mode === 'navigate' || e.request.destination === 'document') {
+    e.respondWith(
+      fetch(e.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  // 静态资源：缓存优先
   e.respondWith(
     caches.match(e.request).then((hit) => hit || fetch(e.request))
   );
